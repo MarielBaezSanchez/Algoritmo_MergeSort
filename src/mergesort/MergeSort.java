e3cdf4845001befb5680f842654f37f9ef947009
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-11-15 11:25:22 -0600

    Inicio de code

---
+++ src/mergesort/MergeSort.java
@@ -0,0 +1,13 @@
+package mergesort;
+
+public class MergeSort {
+    //ordenar el arreglo de numéros A
+    public int [] mergeSort(int [] A){
+
+        return new int[5];
+    }
+    public int [] Merge(int a, int [] b){
+
+        return new int[5];
+    }
+}
