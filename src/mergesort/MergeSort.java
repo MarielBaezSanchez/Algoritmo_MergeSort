76cc540d47d9787a5135cb7d1d7fccddbede5815
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-11-15 11:43:13 -0600

    Segunda modififcación

--- src/mergesort/MergeSort.java
+++ src/mergesort/MergeSort.java
@@ -3,10 +3,24 @@ package mergesort;
 public class MergeSort {
     //ordenar el arreglo de numéros A
     public int [] mergeSort(int [] A){
+        int[] izq;
+        int[] derc;
+        izq = new int [A.length/2];
+
+        if(A.length % 2 == 0){
+            izq = new int [A.length/2];
+        }else {
+            izq = new int[(A.length/2 + 1)];
+        }
+
+        for ({
+
+             })
 
         return new int[5];
     }
-    //recibe dos subarrays y los une en
+    //recibe dos subarrays y los une en unno ordenado
+    // todo, "a y b" deben estar ordenados
     public int [] Merge(int a, int [] b){
 
         return new int[5];
